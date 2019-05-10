e37b222504fa5664a118d489fbb2548d954d7e32
neep305 <neep305@gmail.com>
2019-05-11 00:52:44 +0900

    add PEP-0263

--- NoBatchMain.py
+++ NoBatchMain.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from Crawler import MbsCrawler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
